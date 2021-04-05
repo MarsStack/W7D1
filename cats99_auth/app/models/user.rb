23d671346709e583b5a5c33f152497531990c9c4
Samantha Lilly <samanthalillydesign@gmail.com>
2021-04-05 15:13:13 -0400

    Add methods in user model

--- cats99_auth/app/models/user.rb
+++ cats99_auth/app/models/user.rb
@@ -4,6 +4,26 @@ class User < ApplicationRecord
     validates :password, length: {minimum: 6, allow_nil: true}
 
     after_initialize :ensure_session_token
+    #^do method for this
     
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+        #one is class of string, one is hash class
+        #is_password? Bcrypt built in method is used on strings?? (I think)    
+    end
+
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token   
+    end
+
 
 end 
